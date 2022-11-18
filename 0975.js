b3269c7a9596cd16b036ed1f8d22a29bb23ac0f9
Анастасия <116024029+BelyakovaAV@users.noreply.github.com>
2022-11-18 12:50:34 +0300

    Работает не дальше 3стр, есть ветка целевого сайта

--- 0975.js
+++ 0975.js
@@ -1,10 +1,7 @@
-// ==UserScript==
-// @name         Google Bot
-// @namespace    http://tampermonkey.net/
-// @version      0.1
 // @description  Bot for Google
 // @author       Sergey Chizhikov
 // @match        https://www.google.com/*
+// @match        https://napli.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -12,19 +9,49 @@
 let links = document.links;
 let btnK = document.getElementsByName("btnK")[0];
 let keywords = ["10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress", "Вывод произвольных типов записей и полей в WordPress"];
+let keywords = ["Установка и настройка Git", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress", "Вывод произвольных типов записей и полей в WordPress"];
 let keyword = keywords[getRandom(0, keywords.length)];
+let googleInput = document.getElementsByName("q")[0];
 
 if (btnK !== undefined) {
   document.getElementsByName("q")[0].value = keyword;
   btnK.click();
+  let i = 0;
+  let timerId = setInterval (()=>{
+    googleInput.value += keyword[i];
+    i++;
+    if(i == keyword.length) {
+      clearInterval(timerId);
+      btnK.click();
+    }
+  },500);
+
+
+} else if (location.hostname == "napli.ru") {
+  console.log("Мы на целевом сайте!");
 } else {
+  let nextGooglePage = true;
   for(let i = 0; i < links.length; i++) {
     if (links[i].href.indexOf("napli.ru") !== -1) {
       let link = links[i];
+      nextGooglePage = false;
       console.log("Нашел строку " + link);
       link.click();
+      setTimeout(()=>{
+        link.click();
+      }, getRandom(1500, 4000));
       break;
     }
+  }
+  if (document.querySelector(".YyVfkd").innerText == "3") {
+    nextGooglePage = false;
+    location.href = "https://www.google.com/";
+  }
+  if (nextGooglePage) {
+    setTimeout(()=>{
+      pnnext.click();
+    }, getRandom(2000, 4000))
+
   }
 }
 
