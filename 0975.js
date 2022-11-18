994aefa5f024a7678232401feaa4dabf3f1786f9
Анастасия <116024029+BelyakovaAV@users.noreply.github.com>
2022-11-18 12:52:10 +0300

    Работает на целевом, ходит в выдаче до 5стр.

--- 0975.js
+++ 0975.js
@@ -8,53 +8,59 @@
 
 let links = document.links;
 let btnK = document.getElementsByName("btnK")[0];
-let keywords = ["10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress", "Вывод произвольных типов записей и полей в WordPress"];
-let keywords = ["Установка и настройка Git", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress", "Вывод произвольных типов записей и полей в WordPress"];
+let keywords = [
+	"Установка и настройка Git",
+	"10 самых популярных шрифтов от Google",
+	"Отключение редакций и ревизий в WordPress",
+	"Вывод произвольных типов записей и полей в WordPress",
+];
 let keyword = keywords[getRandom(0, keywords.length)];
 let googleInput = document.getElementsByName("q")[0];
 
 if (btnK !== undefined) {
-  document.getElementsByName("q")[0].value = keyword;
-  btnK.click();
-  let i = 0;
-  let timerId = setInterval (()=>{
-    googleInput.value += keyword[i];
-    i++;
-    if(i == keyword.length) {
-      clearInterval(timerId);
-      btnK.click();
-    }
-  },500);
-
+	let i = 0;
+	let timerId = setInterval(() => {
+		googleInput.value += keyword[i];
+		i++;
+		if (i == keyword.length) {
+			clearInterval(timerId);
+			btnK.click();
+		}
+	}, 500);
 
 } else if (location.hostname == "napli.ru") {
-  console.log("Мы на целевом сайте!");
-} else {
-  let nextGooglePage = true;
-  for(let i = 0; i < links.length; i++) {
-    if (links[i].href.indexOf("napli.ru") !== -1) {
-      let link = links[i];
-      nextGooglePage = false;
-      console.log("Нашел строку " + link);
-      link.click();
-      setTimeout(()=>{
-        link.click();
-      }, getRandom(1500, 4000));
-      break;
-    }
-  }
-  if (document.querySelector(".YyVfkd").innerText == "3") {
-    nextGooglePage = false;
+	//console.log("Мы на целевом сайте!");
+  setInterval(() => {
+  let index = getRandom(0, links.length);
+  if (getRandom(0, 101) > 70) {
     location.href = "https://www.google.com/";
   }
-  if (nextGooglePage) {
-    setTimeout(()=>{
-      pnnext.click();
-    }, getRandom(2000, 4000))
-
-  }
+  if (links[index].href.indexOf("napli.ru") !== -1) links[index].click();
+  }, getRandom(3000, 5000));
+} else {
+	let nextGooglePage = true;
+	for (let i = 0; i < links.length; i++) {
+		if (links[i].href.indexOf("napli.ru") !== -1) {
+			let link = links[i];
+			nextGooglePage = false;
+			console.log("Нашел строку " + link);
+			setTimeout(() => {
+				link.click();
+			}, getRandom(2000, 3000));
+			break;
+		}
+	}
+	if (document.querySelector(".YyVfkd").innerText == "5") {
+		nextGooglePage = false;
+		location.href = "https://www.google.com/";
+	}
+	if (nextGooglePage) {
+		setTimeout(() => {
+			pnnext.click();
+		}, getRandom(2000, 4000));
+	}
 }
 
 function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+	return Math.floor(Math.random() * (max - min) + min);
 }
